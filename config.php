8155b9f599153953c775bab7238414e25c5afe60
Birkhoff Lee <b@irkhoff.com>
2014-10-26 00:39:53 +0800

    Small changes

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+// In freenode server, ports can be 6665, 
+// 6666, 6667, 6697 (SSL only), 7000 (SSL only),
+// 7070 (SSL only), 8000, 8001 and 8002.
 $config = array( 
         'server'  => 'chat.freenode.net', 
         'port'    => 6665, 
