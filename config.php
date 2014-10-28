a07f838aa492923ac8c26f7d984dc93276a76307
Birkhoff Lee <b@irkhoff.com>
2014-10-28 22:04:34 +0800

    Version changing

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
-// In freenode server, ports can be 6665, 
+// In freenode server, ports available are 6665, 
 // 6666, 6667, 6697 (SSL only), 7000 (SSL only),
 // 7070 (SSL only), 8000, 8001 and 8002.
+
 $config = array( 
-        'server'  => 'chat.freenode.net', 
-        'port'    => 6665, 
+        'server'  => 'weber.freenode.net', 
+        'port'    => 8000, 
         'channel' => '#ysitd',
         'name'    => 'botOS', 
         'nick'    => 'botOS', 
         'pass'    => '',
         'admin'   => 'admin',
-        'version' => '2.1.9 build 20141026'
+        'version' => '2.2.1 build 20141028'
 );
