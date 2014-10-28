8692428ee76ec41ec135ef518382069b39113b77
Birkhoff Lee <b@irkhoff.com>
2014-10-28 22:00:15 +0800

    BIG BUG FIX!

--- cmd/admin/channel.php
+++ cmd/admin/channel.php
@@ -11,18 +11,23 @@ if(isset($arguments[1])){
     } else {
         $newChannel = $arguments[1];
     }
-	$content = str_replace($config['channel'], $newChannel, $content);
-    $fileName = "{$root}config.php";
-    $file = fopen($fileName, "w");
-    fwrite($file, $content);
-    fclose($file);
+    if($newChannel == $config['channel']){
+        $this->say('[System] 我本來就在 ' . $newChannel . ' 了呀w', 'Error');
+    } else {
+        $content = str_replace($config['channel'], $newChannel, $content);
+        $fileName = "{$root}config.php";
+        $file = fopen($fileName, "w");
+        fwrite($file, $content);
+        fclose($file);
 
-    $this->say('[System] botOS 將重新啟動以更換頻道至 ' . $newChannel . '。', 'Error', $config['channel']);
-    $this->say('[System] botOS is restarting by ' . $this->SayName . '\'s permission.', 'Error', $config['channel']);
-    $this->say('[System] botOS is restarting.', 'Error', $config['admin']);
-    $this->send_data('QUIT', 'Restart');
-    echo "<meta http-equiv=\"refresh\" content=\"0.1\">";
-    exit;
+        $this->say('[System] botOS 將重新啟動以更換頻道至 ' . $newChannel . '。', 'Error', $config['channel']);
+        $this->say('[System] botOS is restarting by ' . $this->SayName . '\'s permission.', 'Error', $config['channel']);
+        $this->say('[System] botOS 將重新啟動以更換頻道至 ' . $newChannel . '。', 'Error', $config['admin']);
+        $this->say('[System] botOS is restarting.', 'Error', $config['admin']);
+        $this->send_data('QUIT', 'Restart');
+        echo "<meta http-equiv=\"refresh\" content=\"0.1\">";
+        exit;
+    }
 } else {
     $this->say('參數錯誤! 用法: ~channel <頻道名>', 'Error');
 }
