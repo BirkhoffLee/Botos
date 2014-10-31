044c39ba70ae6b7f419f49ce145c9702f60a12dd
Birkhoff Lee <b@irkhoff.com>
2014-11-01 02:29:24 +0800

    Debug command deleted

--- cmd/extra/google_short_expand.php
+++ cmd/extra/google_short_expand.php
@@ -1,6 +1,5 @@
 <?php
 $result = json_decode(file_get_contents('https://www.googleapis.com/urlshortener/v1/url?shortUrl=' . $this->SayContent), true);
-var_dump($result);
 if(@$result['status'] == 'OK'){
 	$this->say($this->SayContent . ' 的原網址: ' . $result['longUrl']);
 }
