0f368399ff3b22a06cbeaeed7e4ee2c1757c5f74
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:54:31 +0800

    New version

--- cmd/admin/restart.php
+++ cmd/admin/restart.php
@@ -1,6 +1,6 @@
 <?php
 $this->say('[System] Botos is restarting by ' . $this->SayName . '\'s permission.', 'Error', $config['channel']);
 $this->say('[System] Botos is restarting.', 'Error', $config['admin']);
-$this->send_data('QUIT', 'Restart');
-echo "<meta http-equiv=\"refresh\" content=\"0.1\">";
+$this->send_data('QUIT Reboot', false);
+echo "<script type='text/javascript'>parent.location.reload();</script>";
 exit;
