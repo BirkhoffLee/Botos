1ad0329050c10e3c64b3b1f78063daa187a7b474
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:56:18 +0800

    BUG FIXING

--- cmd/user/ip.php
+++ cmd/user/ip.php
@@ -1,8 +1,26 @@
 <?php
-$ip = $arguments[1];
-$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $arguments[1]));
-if($query && $query['status'] == 'success') {
-  $this->say('[IP位置查詢] IP: ' . $arguments[1] . '國家: ' . $query['country'] . ' 城市：' . $query['city']);
+global $config;
+if($arguments[1] == ''){
+	$this->say('參數錯誤！用法：~ip <ipaddress>', 'Error');
 } else {
-  $this->say('IP 資料獲取失敗', 'Error');
+	$ip = explode('://', trim($arguments[1]));
+	$ip = str_ireplace('/', '', str_ireplace('\\', '', $ip[0]));
+	$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip));
+	if(@$query && @$query['status'] == 'success') {
+		if(@$query['country'] == ''){
+			$this->say('IP 資料獲取失敗', 'Error');
+		} else {
+			if(@$query['city'] == ''){
+				$Location = ' 國家: ' . $query['country'];
+			} else {
+				$Location = ' 國家: ' . $query['country'] . ' 城市: ' . $query['city'];
+			}
+		    foreach ($config['ipLocations'] as $key => $value) {
+		        $Location = str_ireplace($key, $value, $Location);
+		    }
+			$this->say('[IP位置查詢] IP: ' . $ip . $Location);
+		}
+	} else {
+		$this->say('IP 資料獲取失敗', 'Error');
+	}
 }
