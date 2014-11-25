d0db240aa2ea4d74fb4015bb584a3ec3d46a85d5
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:54:10 +0800

    BUG FIXING

--- cmd/admin/shutdown.php
+++ cmd/admin/shutdown.php
@@ -2,5 +2,5 @@
 $this->say('[System] Botos is shutting down by ' . $this->SayName . '\'s permission.', 'Error', $config['channel']);
 $this->say('[System] Botos is shutting down.', 'Error', $config['admin']);
 $this->Submitlog('[System] Botos shutted down by ' . $this->SayName);
-$this->send_data('QUIT', 'Shutdown');
+$this->send_data('QUIT Shutdown', false);
 exit;
