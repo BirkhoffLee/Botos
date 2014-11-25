af98ae5d894b8f7e718181ffabd02e4c6062ef92
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:56:46 +0800

    More features..

--- cmd/user/rand.php
+++ cmd/user/rand.php
@@ -1,2 +1,4 @@
 <?php
-$this->say('亂數: ' . rand($arguments[1], $arguments[2]));
+$rand1 = (!isset($arguments[1]) or $arguments[1] == '') ? rand(1, 99999) : $arguments[1];
+$rand2 = (!isset($arguments[2]) or $arguments[2] == '') ? rand(1, 99999) : $arguments[2];
+$this->say('亂數: ' . rand($rand1, $rand2));
