7bb05df439fd63da54819adb52c2de35a356ef5f
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:54:45 +0800

    New UI!

---
+++ cmd/extra/main.header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <title><?php echo $title;?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+        body{
+        	color: #28fe14;
+            background-color: black;
+            font-family: "Courier New";
+            font-size: 15pt;
+            cursor: default
+        }
+    </style>
+    <script type="text/javascript">
+    document.ondragstart = function(){return false;}
+    document.oncontextmenu = function(){return false;}
+    document.onselectstart = function(){return false;}
+    </script>
+    </head>
+<body>
