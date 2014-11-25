838136ac5f190b258a67cb80c13ccd5482b9db46
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:57:05 +0800

    New version

--- config.php
+++ config.php
@@ -6,11 +6,33 @@
 $config = array( 
         'server'  => 'weber.freenode.net', 
         'port'    => 8000, 
-        'channel' => '#Botos',
+        'channel' => '#ysitd',
         'name'    => 'Botos', 
         'nick'    => 'Botos', 
         'pass'    => '',
-        'admin'   => 'admin',
-        'version' => '2.2.2 build 20141102',
-        'checkUpdate' => true
+        'admin'   => 'Admin',
+        'version' => '2.2.3 build 20141125',
+        'checkUpdate' => true,
+        'ipLocations' => array(
+        	'Taiwan' => '臺灣',
+            'China' => '中國大陸',
+            'HongKong' => '香港',
+            'United States' => '美國',
+            'South Africa' => '南非',
+        	'Taipei' => '臺北',
+            'Taoyuan' => '桃園',
+            'Beijing' => '北京',
+            'Shenzhen' => '深圳',
+            'Hangzhou' => '杭州',
+            'Guangzhou' => '廣州',
+            'New York' => '紐約',
+            'Seattle' => '西雅圖',
+            'Fremont' => '費利蒙',
+            'Reston' => '雷斯頓',
+            'San Francisco' => '舊金山',
+            'Menlo Park' => '門洛帕克',
+            'Mountain View' => '山景城',
+            'Newark' => '紐華克',
+            'Los Angeles' => '洛杉磯'
+        )
 );
