73c77bd4b414bd9841d80a3a80933b91e53e5e2c
Birkhoff Lee <b@irkhoff.com>
2014-11-25 19:56:07 +0800

    BUG FIXING

--- cmd/user/info.php
+++ cmd/user/info.php
@@ -3,28 +3,30 @@ global $startTime;
 global $config;
 $time = round(microtime(true) - $startTime);
 $value = array( 
-  "years" => 0, "days" => 0, "hours" => 0, 
-  "minutes" => 0, "seconds" => 0, 
+    "years" => 0, "days" => 0, "hours" => 0, 
+    "minutes" => 0, "seconds" => 0, 
 ); 
 if($time >= 31556926){ 
-  $value["years"] = floor($time/31556926); 
-  $time = ($time%31556926); 
+    $value["years"] = floor($time/31556926); 
+    $time = ($time%31556926); 
 } 
 if($time >= 86400){ 
-  $value["days"] = floor($time/86400); 
-  $time = ($time%86400); 
+    $value["days"] = floor($time/86400); 
+    $time = ($time%86400); 
 } 
 if($time >= 3600){ 
-  $value["hours"] = floor($time/3600); 
-  $time = ($time%3600); 
+    $value["hours"] = floor($time/3600); 
+    $time = ($time%3600); 
 } 
 if($time >= 60){ 
-  $value["minutes"] = floor($time/60); 
-  $time = ($time%60); 
+    $value["minutes"] = floor($time/60); 
+    $time = ($time%60); 
 } 
 $value["seconds"] = floor($time);
 $uptime = "{$value['days']} 天 {$value['hours']} 小時 {$value['minutes']} 分鐘 {$value['seconds']} 秒";
-$this->say($this->SayName . ': 系統資訊已使用悄悄話方式傳送。');
+if($this->SayTarget != $config['nick']){
+    $this->say($this->SayName . ': 更新日誌已使用悄悄話方式傳送。');
+}
 $this->say('Botos by Birkhoff >>>>>>>>>>>>>>>>>>>>>資訊', 'Information', $this->SayName);
 $this->say('===========================================', 'Information', $this->SayName);
 $this->say("連線至伺服器: {$config['server']}:{$config['port']}", 'Information', $this->SayName);
